58862028bf31b89e2a4311e359cf1b0bb0aa0d1b
AngelLiang <pl01665077@163.com>
2022-09-29 21:58:43 +0800

    refactor: 更新 startproject 模板

---
+++ startproject_template/config/urls_swagger.py
@@ -0,0 +1,40 @@
+from django.urls import path
+from django.conf.urls import url
+from rest_framework.authentication import SessionAuthentication
+
+# from .permissions import IsSuperuser
+from rest_framework.permissions import IsAuthenticated
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from django.views import static
+from django.conf import settings
+from .urls import api_urlpatterns as patterns
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django",
+        default_version='v1',
+        # description="Test description",
+    ),
+    public=True,
+    # authentication_classes=(
+    #     SessionAuthentication,
+    # ),
+    # permission_classes=(
+    #     IsAuthenticated,
+    # ),
+    patterns=patterns,
+)
+
+urlpatterns = [
+    # for drf_yasg
+    url(r'^lazy/(?P<path>.*)$', static.serve,
+        {'document_root': settings.STATIC_ROOT}, name='static'),
+    url(r'^api/v1/swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='api-v1-json'),
+    url(r'^api/v1/swagger/index.html',
+        schema_view.with_ui('swagger', cache_timeout=0), name='api-v1-ui'),
+]
